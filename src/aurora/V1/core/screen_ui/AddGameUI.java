eb9eab9ef71b2a1dcdfe654ff44ed75037e066b7
Sammy <sguergachi@gmail.com>
2014-05-23 00:23:25 -0400

    UI Fix

--- src/aurora/V1/core/screen_ui/AddGameUI.java
+++ src/aurora/V1/core/screen_ui/AddGameUI.java
@@ -646,8 +646,6 @@ public class AddGameUI {
             }
 
             // Add Go To Program
-            pnlRightOfTopEastContainer.add(Box.createHorizontalStrut(
-                    10));
             pnlRightOfTopEastContainer.add(btnGoToProgram);
 
             pnlRightOfTopEast.add(Box.createHorizontalStrut(pnlRightOfTop
